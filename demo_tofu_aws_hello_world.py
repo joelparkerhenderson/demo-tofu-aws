5bcdd997e51dac0becba7c1c10d3570cec8b413b
Joel Parker Henderson <joel@joelparkerhenderson.com>
2025-01-19 22:35:10 -0800

    Redo hello world to use API

--- demo_tofu_aws_hello_world.py
+++ demo_tofu_aws_hello_world.py
@@ -1,2 +1,19 @@
+# AWS lambda hander that returns "hello world".help
+# 
+# Thanks to the AWS developer guide pages:
+#
+# https://docs.aws.amazon.com/apigateway/latest/developerguide/getting-started.html
+#
+# https://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-create-api-as-simple-proxy-for-lambda.html
+
+import json
+
 def lambda_handler(event, context):
-    print("Hello World")
+    print(event)
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "*/*"
+        },
+        "body": "hello world"
+    }
