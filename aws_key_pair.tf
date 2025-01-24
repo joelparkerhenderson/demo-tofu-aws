9fb3c3d9db521354f534a7b8b1e87a47a4ad9001
Joel Parker Henderson <joel@joelparkerhenderson.com>
2025-01-24 07:47:48 -0800

    Fix name

--- aws_key_pair.tf
+++ aws_key_pair.tf
@@ -7,7 +7,7 @@
 # instances.
 ##
 
-resource "aws_key_pair" "demo_tofu_key_pair" {
+resource "aws_key_pair" "demo_tofu_aws_key_pair" {
   key_name   = "demo_tofu_aws_key_pair"
   public_key = file("demo_tofu_aws_key_pair.pub")
 }
