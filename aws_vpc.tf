6bc0d526a3ba022451bd597660d3a1481cd8f0db
Joel Parker Henderson <joel@joelparkerhenderson.com>
2025-01-26 23:45:56 -0800

    Add tigher CIDR to aws_vpc.tf

--- aws_vpc.tf
+++ aws_vpc.tf
@@ -4,10 +4,12 @@
 # Amazon Virtual Private Cloud (VPC) is a virtual network that allows users to
 # launch Amazon Web Services (AWS) resources in a logically isolated section of
 # the AWS cloud. After you create a VPC, you can add subnets.
+#
+# This is the platform on which we host the entire demo infrastructure. 
 ##
 
 resource "aws_vpc" "demo_tofu_aws" {
-  cidr_block           = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/24"
   enable_dns_hostnames = true
   tags = {
     Name = "demo_tofu_aws"
