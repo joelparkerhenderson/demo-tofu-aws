18af7a44165104570f227fc401484f9c1cd01871
Joel Parker Henderson <joel@joelparkerhenderson.com>
2025-01-26 23:47:24 -0800

    Add aws_network_interface.tf availability zone

--- aws_network_interface.tf
+++ aws_network_interface.tf
@@ -5,8 +5,8 @@
 # Uncomment this if you wish to create your own aws network interface.
 # This demo doesn't need this because AWS automatically creates it for us.
 
-resource "aws_network_interface" "demo_tofu_aws" {
-  subnet_id       = aws_subnet.demo_tofu_aws.id
+resource "aws_network_interface" "demo_tofu_aws_availability_zone_0" {
+  subnet_id       = aws_subnet.demo_tofu_aws_availability_zone_0.id
   private_ips     = ["10.0.0.50"]
   security_groups = [aws_security_group.demo_tofu_aws.id]
 
@@ -16,7 +16,23 @@ resource "aws_network_interface" "demo_tofu_aws" {
   }
 
   tags = {
-    Name = "demo_tofu_aws"
+    Name = "demo_tofu_aws_availability_zone_0"
   }
 
 }
+
+# resource "aws_network_interface" "demo_tofu_aws_availability_zone_1" {
+#   subnet_id       = aws_subnet.demo_tofu_aws_availability_zone_1.id
+#   private_ips     = ["10.0.0.50"]
+#   security_groups = [aws_security_group.demo_tofu_aws.id]
+#
+#   attachment {
+#     instance = aws_instance.demo_tofu_aws.id
+#     device_index = 1
+#   }
+#
+#   tags = {
+#     Name = "demo_tofu_aws_availability_zone_1"
+#   }
+#
+# }
