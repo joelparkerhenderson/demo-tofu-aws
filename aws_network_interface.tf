9d966dac96f248737446011e29c546d6b4763979
Joel Parker Henderson <joel@joelparkerhenderson.com>
2025-01-26 18:02:04 -0800

    Add aws_network_interface.tf

--- aws_network_interface.tf
+++ aws_network_interface.tf
@@ -5,13 +5,18 @@
 # Uncomment this if you wish to create your own aws network interface.
 # This demo doesn't need this because AWS automatically creates it for us.
 
-# resource "aws_network_interface" "demo_tofu_aws_network_interface" {
-#   subnet_id       = aws_subnet.public_a.id
-#   private_ips     = ["10.0.0.50"]
-#   security_groups = [aws_security_group.demo_tofu_aws_security_group.id]
-#
-#   attachment {
-#     instance = aws_instance.test.id
-#     device_index = 1
-#   }
-# }
+resource "aws_network_interface" "demo_tofu_aws" {
+  subnet_id       = aws_subnet.demo_tofu_aws.id
+  private_ips     = ["10.0.0.50"]
+  security_groups = [aws_security_group.demo_tofu_aws.id]
+
+  attachment {
+    instance = aws_instance.demo_tofu_aws.id
+    device_index = 1
+  }
+
+  tags = {
+    Name = "demo_tofu_aws"
+  }
+
+}
