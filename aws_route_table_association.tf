7766f3b6c59a3af13e7ac1087cfb48e4f6a4c51e
Joel Parker Henderson <joel@joelparkerhenderson.com>
2025-02-03 09:16:40 -0800

    Add aws_route_table_association.tf

---
+++ aws_route_table_association.tf
@@ -0,0 +1,10 @@
+##
+# AWS route table association
+#
+# Provides a resource to create an association between a route table and a subnet or a route table and an internet gateway or virtual private gateway.
+##
+
+resource "aws_route_table_association" "demo_tofu_aws" {
+  gateway_id     = aws_internet_gateway.demo_tofu_aws.id
+  route_table_id = aws_route_table.demo_tofu_aws.id
+}
